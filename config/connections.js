82bedfc804c6d02f38cad9391c769d95037b6546
Chieftjz <taylor.zlomke@gmail.com>
2019-02-03 12:07:15 -0700

    updated connection.js

--- config/connections.js
+++ config/connections.js
@@ -1,7 +1,7 @@
 var mysql = require("mysql");
 
 if (process.env.JAWSDB_URL) {
-    var connection = mysql.createConnection(process.env.JAWSDB_URL)
+    var connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     var connection = mysql.createConnection({
         host: "localhost",
@@ -10,8 +10,7 @@ if (process.env.JAWSDB_URL) {
         password: "Ildhcmfam226!",
         database: "burgers_db"
     });
-}
-
+};
 
 connection.connect(function (err) {
     if (err) {
